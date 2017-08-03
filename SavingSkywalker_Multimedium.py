155dac0c554bd922682e6c17eea0eb93f7cf0d19
Josh <jkirker@hawaii.edu>
2017-08-02 16:46:22 -1000

    Function Variables
    Set certain global variables whos values are defined in answer(); not certain if that's the best method.
    
    Placed some comments below the ToDo list in order to see the c_c_c function better.
    
    Still not certain what I should do about the cooling_const = c_c_c(radius) line.  It also states that (radius) is undefined, but it still works and without the line it doesn't.

--- SavingSkywalker_Multimedium.py
+++ SavingSkywalker_Multimedium.py
@@ -1,63 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
-'''
-N=10000
-M=10
-tlim = (0.,24.)
-Ti_LS = 35.
-Ti_tt = 37.
-#k_LS = (500*1.9/(70*4186)*60)
-#k_tt = (100*4.5/(290*205)*60)
-Vm = 0.187249
-day_H=24.
-Tmin_H = -60.
-r_max = 0.310037
-r_max_luke = (1.9/(4*np.pi))**(1/2)
 
-volume_max = 0.187249
-volume_max_luke = ((3*r_max_luke)/(4*np.pi))**(1/2)
-'''
+def T_Hoth(t):
+    global Tmin_H, day_H
+    return 0.5*Tmin_H*(1+np.sin(2*np.pi*t/day_H))
 
 def cooling_constant_computation(radius):## KLC: you need to pass in a radius variable
-    r_max_tt = 0.310037
-    r_max_luke = (1.9/(4*np.pi))**(1/2)
-    volume_max_luke = ((3*r_max_luke)/(4*np.pi))**(1/2)
-    volume_max_tt= 0.187249
+    global r_max_tt, r_max_luke, volume_max_luke, volume_max_tt
     
-    ## KLC: But you also nee an appropriately sized container
+    volume_max_luke = ((3*r_max_luke)/(4*np.pi))**(1/2)
     cooling_constant = np.empty(len(radius))
+    
     for rr in range(len(radius)):
-       ## R = radius[rr] # KLC: doing this way more robust to diff in
-                       # Python v2 vs v3 *and* enables rr to index
-                       # cooling_constant (for R in radius would have
-                       # R not applicable)
         height = r_max_luke + radius[rr]
-        if radius[rr] <= r_max_luke: ##Below are computations for luke
+        if radius[rr] <= r_max_luke:
             Surface_Area = 2*np.pi*radius[rr]*(radius[rr]+height)
-            Volume = (4/3)*np.pi*(radius[rr]**3) ##Volume of a sphere for Luke
+            Volume = (4/3)*np.pi*(radius[rr]**3)
             Mass = 70*(Volume/volume_max_luke)
             cooling_constant[rr] = 60*(500*Surface_Area)/(Mass*4186)
-        elif radius[rr] > r_max_luke and radius[rr] < r_max_tt:  ##Below are computations for tauntaun
+        elif radius[rr] > r_max_luke and radius[rr] < r_max_tt:
             Surface_Area = 2*np.pi*radius[rr]*(radius[rr]+height)
-            Volume = np.pi*(radius[rr]**2)*height ## Volume of a cylinder for tauntaun
+            Volume = np.pi*(radius[rr]**2)*height
             Mass = 290*(Volume/volume_max_tt)
             cooling_constant[rr] = 60*(100*Surface_Area)/(Mass*205)
-    return cooling_constant # KLC: return the array (not within for-loop)
+    return cooling_constant
 
 
-def dTdt(T_now,radius,Tmin_H = -60):
+def dTdt(T_now,T_H_now,Tmin_H = -60):
+    global k_LS, k_tt
+    
     nshells = len(T_now)
     dTdt_now = np.empty(nshells)
-#    dTdt_now[0] = -cooling_constant_computation[0]*(T_now[0] - T_now[1])
-
-    ## KLC: functions are accessed with parentheses, not square
-    ## brackets but it seems you actually want an array of cooling
-    ## constants so since the radius variable is defined and
-    ## instantiated outside any function, it is available to all
-    ## functions. So we'll use the function
-    ## cooling_constant_computation() on the "global-ish" variable
-    ## radius to define an array of cooling constants. Then use it in
-    ## the calculations.
     cooling_const = cooling_constant_computation(radius) ## KLC: but really, pass in or make global variable e.e., k_r
     dTdt_now[0] = -cooling_const[0]*(T_now[0] - T_now[1])
     for ss in range(1,nshells-1): # up to but not including outer shell
@@ -67,27 +40,24 @@ def dTdt(T_now,radius,Tmin_H = -60):
     dTdt_now[-1] = -cooling_const[-1]*(T_now[-1] - Tmin_H)# - T_now[4]) error in T_now[4]: 
     return dTdt_now            
     
-#T_Hoth = 0.5*Tmin_H*(1+np.sin(2*np.pi*time/day_H))
 
-'''
-T_all = np.empty((N,M),float)
-
-## Modeling initial temperature gradient (linear from core to surface)
-T_inner = 37. # C; human core temp
-T_outer = 32. # C; human freezing
-slope = (T_outer - T_inner)/radius[-1] # C/m where origin at radius = 0 
-T_init = slope*radius + T_inner # C
-
-#T_all[0,:] = np.array([37,35,33,31],float) # arbitrary instantiation
-T_all[0,:] = T_init 
-#[0],radius[1],radius[2],radius[3]],float)
- 
-radius = np.linspace(0,0.310037,M+1) # +1 to make M shells
-radius = radius[1:] # want radius of outer parts of shells
-#above line eliminates first place 0, to avoid a division by 0
-'''    
-def answer(Ti_LS = 35., Ti_tt = 37., day_H = 24, tlim = (0.,24.), Tmin_H = -60,
+def answer(RM_tt = 0.310037, RM_LS = (1.9/(4*np.pi))**(1/2), \
+           VM_tt = 0.187249, Ti_LS = 35., Ti_tt = 37., \
+           day_H = 24, tlim = (0.,24.), Tmin_H = -60, \
            N=10000, M=10):
+    '''
+    values for radii max to set global variables
+    volume max of tauntaun set to global variables
+    volume max of luke defined in cooling_constant_computation
+        because it uses r_max_luke    
+    '''
+    global r_max_tt, r_max_luke, volume_max_tt, volume_max_luke
+    
+    r_max_tt = RM_tt
+    r_max_luke = RM_LS
+    volume_max_tt = VM_tt
+
+    
     radius = np.linspace(0,0.310037,M+1) # +1 to make M shells
     radius = radius[1:]
     
@@ -128,13 +98,13 @@ def answer(Ti_LS = 35., Ti_tt = 37., day_H = 24, tlim = (0.,24.), Tmin_H = -60,
 
 T_all = answer()
 
-
+'''
 for ii in range(M):
     plt.plot(time,T_all[:,ii],label='M = {0}'.format(ii))
 #plt.semilogy()
 plt.legend()
 plt.show()
-
+'''
 
 
 
@@ -146,3 +116,20 @@ plt.show()
 ## - add tauntaun shell (represented by diff. cooling constant array)
 ## - add constant or variable Hoth temperature
 ## - enable user to change geometry (i.e., enable "spherical" mode)
+
+
+
+'''
+#    dTdt_now[0] = -cooling_constant_computation[0]*(T_now[0] - T_now[1])
+
+    ## KLC: functions are accessed with parentheses, not square
+    ## brackets but it seems you actually want an array of cooling
+    ## constants so since the radius variable is defined and
+    ## instantiated outside any function, it is available to all
+    ## functions. So we'll use the function
+    ## cooling_constant_computation() on the "global-ish" variable
+    ## radius to define an array of cooling constants. Then use it in
+    ## the calculations.
+
+#T_Hoth = 0.5*Tmin_H*(1+np.sin(2*np.pi*time/day_H))
+'''
